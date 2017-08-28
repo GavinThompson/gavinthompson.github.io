4696b056185822237e704f801e7be5fedbd33aab
Gavin Thompson <gavinathompson@gmail.com>
2017-08-27 20:42:48 -0400

    Adding copy to each project; deleting buttons/mini-menus; css tweaks; adding links and skills to each item

--- js/slideshow.js
+++ js/slideshow.js
@@ -16,22 +16,14 @@ var curIndex = 0,
         ];
 
 
-//
-// Dynamically add each image frame into the dom;
-//
 function buildSlideShow(arr) {
     for (i = 0; i < arr.length; i++) {
         var img = document.createElement('img');
         img.src = arr[i];
         slideshow.appendChild(img);
     }
-    // note the slides reference will now contain the images so we can access them
 }
 
-//
-// Our slideshow function, we can call this and it flips the image instantly, once it is called it will roll
-// our images at given interval [imgDuration];
-//
 function slideShow() {
     
     function fadeIn(e) {
@@ -42,7 +34,6 @@ function slideShow() {
         e.className = "";
     };
 
-        // debugger;
 
         fadeOut(slides[curIndex]);
         curIndex++;
@@ -58,4 +49,5 @@ function slideShow() {
     };
     buildSlideShow(imgArray);
     slideShow();
-    console.log("show")
+
+
